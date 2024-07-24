9b33da8f87b2a8e4f0b4fc0cef8943b4f47f521a
lucasberg29 <lucasberg29@gmail.com>
2024-07-23 20:19:30 -0400

    putting site live

--- portfolio_react_site/src/App.js
+++ portfolio_react_site/src/App.js
@@ -1,29 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './HomePage.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
-    </div>
-    </div>
-
-    
+    <HomePage/>
   );
 }
 
