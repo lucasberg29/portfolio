c9b44f7177a166a3db250cb93bcbf3860005ac8d
lucasberg29 <lucasberg29@gmail.com>
2024-08-10 19:47:21 -0400

    added new version of flappy duck and added functions to pause or resume game

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -24,7 +24,7 @@ const Navigation = () => {
                 </div>
             </nav>
         </div>
-  );
+    );
 };
 
 export default Navigation;
--- src/components/UnityGame.jsx
+++ src/components/UnityGame.jsx
@@ -6,16 +6,27 @@ function GamesPage({gameName}) {
     const baseURL = import.meta.env.VITE_BASE_URL;
     const gamePath = `${baseURL}games/${gameName}/Build/`
 
-    const {unityProvider} = useUnityContext({
-        arguments: [],
+    const {unityProvider, sendMessage, addEventListener} = useUnityContext({
         loaderUrl:      gamePath + "WebGL.loader.js",
         dataUrl:        gamePath + "WebGL.data",
         frameworkUrl:   gamePath + "WebGL.framework.js",
         codeUrl:        gamePath + "WebGL.wasm",
     });
 
-    return (        
-        <Unity className='unityGame' unityProvider={unityProvider} />
+    function pauseGame() {
+        sendMessage("_LevelManager", "PauseGame");
+    }
+    
+    function startGame() {
+        sendMessage("_LevelManager", "ResumeGame");
+    }
+
+    return (  
+        <div>
+            <Unity className='unityGame' unityProvider={unityProvider} />
+            <button onClick={startGame}>Start Game</button>
+            <button onClick={pauseGame}>Pause Game</button>
+        </div>      
     ) 
 }
 
