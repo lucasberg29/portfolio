9a1ed65e3bde78625d88d2e014a02d676930dea7
lucasberg29 <lucasberg29@gmail.com>
2024-08-17 17:20:50 -0400

    cleaned gamespage

--- src/pages/GamesPage.jsx
+++ src/pages/GamesPage.jsx
@@ -2,20 +2,15 @@ import React, { useEffect } from 'react';
 import UnityGame from "../components/UnityGame.jsx";
 
 function GamesPage({setSelectedPage}) {
-
     useEffect(() => {
         setSelectedPage('games');
       }, []); 
-
-    //const baseURL = import.meta.env.VITE_BASE_URL;
-    //const gamePath = `${baseURL}games/flappyDuck/Build/`
 
     return (        
         <div className="GamesPage">
             <h1>Games Page</h1>
             <UnityGame gameName="flappyDuck" />
             <hr/>
-            {/* //<UnityGame gameName="flappyDuck" /> */}
         </div>
     ) 
 }
