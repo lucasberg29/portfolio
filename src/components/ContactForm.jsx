704abea87ebf61a96c615bcb3a330c5f07d63240
lucasberg29 <lucasberg29@gmail.com>
2025-08-06 22:43:50 -0400

    changed font and improved contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,96 +1,126 @@
-import React, { useState } from 'react';
-import { TextField, Button, Box, Typography } from '@mui/material';
-import emailjs from 'emailjs-com';
+import React, { useState } from "react";
+import { TextField, Button, Box, Typography } from "@mui/material";
+import emailjs from "emailjs-com";
 
 function ContactForm() {
-   const [name, setName] = useState('');
-   const [email, setEmail] = useState('');
-   const [message, setMessage] = useState('');
-   const [status, setStatus] = useState('');
-   const [responseClass, setResponseClass] = useState('success');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [status, setStatus] = useState("");
+  const [responseClass, setResponseClass] = useState("success");
 
-   const handleSubmit = (event) => {
-      event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-      if (!name || !email || !message) {
-         setStatus('Please fill out all fields.');
-         setResponseClass('warning');
-         return;
-      }
+    if (!name || !email || !message) {
+      setStatus("Please fill out all fields.");
+      setResponseClass("warning");
+      return;
+    }
 
-      const templateParams = {
-         from_name: name,
-         message: message,
-         user_email: email,
-         reply_to: email,
-      };
+    const templateParams = {
+      from_name: name,
+      message: message,
+      user_email: email,
+      reply_to: email,
+    };
 
-      emailjs.send('service_lvgtz8h', 'template_p5gpgwg', templateParams, 'QkssPB9PI7OzDeDPs')
-         .then((response) => {
-            console.log('SUCCESS!', response.status, response.text);
-            setStatus('Message sent successfully!');
-            setResponseClass('success');
-            setName('');
-            setEmail('');
-            setMessage('');
+    emailjs
+      .send(
+        "service_lvgtz8h",
+        "template_p5gpgwg",
+        templateParams,
+        "QkssPB9PI7OzDeDPs"
+      )
+      .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+        setStatus("Message sent successfully!");
+        setResponseClass("success");
+        setName("");
+        setEmail("");
+        setMessage("");
       })
-         .catch((error) => {
-            console.log('FAILED: ', error);
-            setStatus('Failed to send message. Please try again.');
-            setResponseClass('failed');
+      .catch((error) => {
+        console.log("FAILED: ", error);
+        setStatus("Failed to send message. Please try again.");
+        setResponseClass("failed");
       });
-   };
+  };
 
-   return (
-      <Box sx={{ maxWidth: 600, mx: 'auto', p: 1, bgcolor: 'background.paper', borderRadius: 2, boxShadow: 3 }}>
-         <Typography variant="h4" component="h1" gutterBottom>
-            Contact
-         </Typography>
-         <form onSubmit={handleSubmit}>
-            <TextField fullWidth label="Name"value={name}
-               onChange={(e) => setName(e.target.value)}
-               variant="outlined"
-               margin="normal"
-               required
+  return (
+    <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div id="nameAndEmailContactForm">
+            <TextField
+              className="customInputField"
+              fullWidth
+              label="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              variant="outlined"
+              margin="normal"
+              required
             />
             <TextField
-               fullWidth
-               label="Email"
-               type="email"
-               value={email}
-               onChange={(e) => setEmail(e.target.value)}
-               variant="outlined"
-               margin="normal"
-               required
+              className="customInputField"
+              fullWidth
+              label="Email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              variant="outlined"
+              margin="normal"
+              required
             />
-            <TextField
-               fullWidth
-               label="Message"
-               value={message}
-               onChange={(e) => setMessage(e.target.value)}
-               variant="outlined"
-               margin="normal"
-               required
-               multiline
-               rows={4}
-            />
-            <Button
-               type="submit"
-               variant="contained"
-               color="primary"
-               fullWidth
-               sx={{ mt: 3 }}
-            >
-               Send
-            </Button>
-         </form>
-         {status && (
-         <Typography className={`statusMessage ${responseClass}`} variant="body1" color="textSecondary" align="center" sx={{ mt: 2 }}>
+          </div>
+          <TextField
+            className="customInputField"
+            fullWidth
+            label="Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            size="small"
+            sx={{
+              mt: 3,
+              px: 10,
+              py: 1.5,
+              fontSize: "0.75rem",
+              fontFamily: "monospace",
+              backgroundColor: "teal",
+              color: "white",
+              borderRadius: "50px",
+              mx: "auto",
+              display: "block",
+              "&:hover": {
+                backgroundColor: "#008080",
+              },
+            }}
+          >
+            Send
+          </Button>
+        </form>
+        {status && (
+          <Typography
+            className={`statusMessage ${responseClass}`}
+            variant="body1"
+            color="textSecondary"
+            align="center"
+            sx={{ mt: 2 }}
+          >
             {status}
-         </Typography>
-            )}
-      </Box>
-   );
+          </Typography>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default ContactForm;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,125 +1,144 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 import UnityGame from "../components/UnityGame.jsx";
+import ContactForm from "../components/ContactForm.jsx";
 
-import unityLogo from '../assets/icons/unity_logo.png';
-import opengGlLogo from '../assets/icons/openGl_cpp_logo.png';
+import unityLogo from "../assets/icons/unity_logo.png";
+import opengGlLogo from "../assets/icons/openGl_cpp_logo.png";
 
-import gamesData from '../data/games.json';
-import openGlData from '../data/openGlGames.json';
-import { Grid } from '@mui/material';
+import gamesData from "../data/games.json";
+import openGlData from "../data/openGlGames.json";
+import { Grid } from "@mui/material";
 
-function HomePage({setSelectedPage, currentPage}) {
-    const baseURL = import.meta.env.VITE_BASE_URL;
+function HomePage({ setSelectedPage, currentPage }) {
+  const baseURL = import.meta.env.VITE_BASE_URL;
 
-    const [games, setGames] = useState([]);
+  const [games, setGames] = useState([]);
 
-    useEffect(() => {
-        setSelectedPage('home');
-        setGames(gamesData);
-    }, []);
+  useEffect(() => {
+    setSelectedPage("home");
+    setGames(gamesData);
+  }, []);
 
-    function handleGameClick(gameId) {
-        setGames((prevGames) =>
-            prevGames.map((g) =>
-                g.id === gameId ? { ...g, isRunning: true } : g
-            )
-        );
-    }
+  function handleGameClick(gameId) {
+    setGames((prevGames) =>
+      prevGames.map((g) => (g.id === gameId ? { ...g, isRunning: true } : g))
+    );
+  }
 
-  return(
+  return (
     <div className="homePage">
       <div className="introduction">
         <div className="salutation">Hello!</div>
-        <div className="presentation">I'm Lucas Berg, a passionate software developer who loves to design and develop games and interactive applications.</div>
+        <div className="presentation">
+          I'm Lucas Berg, a passionate software developer who loves to design
+          and develop games and interactive applications.
+        </div>
       </div>
-            <div className="unityGamesGrid">
-                <img className="unityLogo" src={unityLogo} alt="Unity logo image" />
-                <Grid container spacing={5}>
-                    {games.map((game,id) => (
-                    <Grid key={id} item xs={12} sm={6} md={3} className="gameGridItem">
-                        <div className="gameCard">
-                            <h1>{game.name}</h1>
-                            <a onClick={() => handleGameClick(game.id)} className="gameCoverClick">
-                            <img
-                                    src={`${baseURL}/games/${game.projectName}/art/gameCover.png`}
-                                    className="gameCover"
-                                    alt="Overlay"
-                            />
-                            </a>
-                            <div className="gameTags">
-                                <a href={game.gameRepo} >
-                                    <div className="gameTag githubTag">Github</div>
-                                </a>
-                            </div>
-                            <div className="gameDescription">{game.description}</div>
-                            <hr />
-                        </div>
-                    </Grid>
-                    ))}
-                </Grid>
-            </div>
-            {games.map((game) =>
-                game.isRunning ? (
-                    <UnityGame key={game.id} gameName={game.projectName} game={game} />
-                ) : null
-            )}
-            <div className="openGlGamesGrid">
-                <img className="openGlLogo" src={opengGlLogo} alt="OpenGL/C++ image" />
-                <Grid container spacing={5}>
-                    {openGlData.map((game,id) => (
-                        <Grid key={id} item xs={12} sm={6} md={3} className="gameGridItem">
-                            <div className="gameCard">
-                                <h1>{game.name}</h1>
-                                <a href="https://lucasberg29.itch.io/galaga-1981" className="gameCoverClick">
-                                    <img
-                                    src={`${baseURL}/openglgames/${game.projectName}/art/gameCover.png`}
-                                    className="gameCover"
-                                    alt="Overlay"
-                                    />
-                                </a>
-                                <div className="gameTags">
-                                    <a href={game.gameRepo} >
-                                    <div className="gameTag githubTag">Github</div>
-                                    </a>
-                                </div>
-                                <div className="gameDescription">{game.description}</div>
-                                <hr />
-                            </div>
-                        </Grid>
-                    ))}
-                </Grid>
-            </div>
+      <div className="unityGamesGrid">
+        <img className="unityLogo" src={unityLogo} alt="Unity logo image" />
+        <Grid container spacing={5}>
+          {games.map((game, id) => (
+            <Grid key={id} item xs={12} sm={6} md={3} className="gameGridItem">
+              <div className="gameCard">
+                <h1>{game.name}</h1>
+                <a
+                  onClick={() => handleGameClick(game.id)}
+                  className="gameCoverClick"
+                >
+                  <img
+                    src={`${baseURL}/games/${game.projectName}/art/gameCover.png`}
+                    className="gameCover"
+                    alt="Overlay"
+                  />
+                </a>
+                <div className="gameTags">
+                  <a href={game.gameRepo}>
+                    <div className="gameTag githubTag">Github</div>
+                  </a>
+                </div>
+                <div className="gameDescription">{game.description}</div>
+                <hr />
+              </div>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+      {games.map((game) =>
+        game.isRunning ? (
+          <UnityGame key={game.id} gameName={game.projectName} game={game} />
+        ) : null
+      )}
+      <div className="openGlGamesGrid">
+        <img className="openGlLogo" src={opengGlLogo} alt="OpenGL/C++ image" />
+        <Grid container spacing={5}>
+          {openGlData.map((game, id) => (
+            <Grid key={id} item xs={12} sm={6} md={3} className="gameGridItem">
+              <div className="gameCard">
+                <h1>{game.name}</h1>
+                <a
+                  href="https://lucasberg29.itch.io/galaga-1981"
+                  className="gameCoverClick"
+                >
+                  <img
+                    src={`${baseURL}/openglgames/${game.projectName}/art/gameCover.png`}
+                    className="gameCover"
+                    alt="Overlay"
+                  />
+                </a>
+                <div className="gameTags">
+                  <a href={game.gameRepo}>
+                    <div className="gameTag githubTag">Github</div>
+                  </a>
+                </div>
+                <div className="gameDescription">{game.description}</div>
+                <hr />
+              </div>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
 
       <div className="sectionTitle">Skills</div>
       <div className="skillSection">
-      <img
-        style={{ width: '32px', height: '24px', float: 'left', marginRight: '8px' }}
-      src="https://flagcdn.com/w320/br.png"
-      alt="Brazil Flag"
-      width="32"
-      height="24"
-    />
-    <div>Native speaker</div>
-  </div>
+        <img
+          style={{
+            width: "32px",
+            height: "24px",
+            float: "left",
+            marginRight: "8px",
+          }}
+          src="https://flagcdn.com/w320/br.png"
+          alt="Brazil Flag"
+          width="32"
+          height="24"
+        />
+        <div>Native speaker</div>
+      </div>
 
-  <div className="skillSection" >
-    <img
-        style={{
-    width: '32px',
-    height: '24px',
-    objectFit: 'cover',
-    float: 'left',
-    marginRight: '8px',
-  }}
-      src="https://flagcdn.com/w320/ca.png"
-      alt="Canada Flag"
-      width="32"
-      height="24"
-    />
-  <div>Fluent in English</div>
-</div>
-
+      <div className="skillSection">
+        <img
+          style={{
+            width: "32px",
+            height: "24px",
+            objectFit: "cover",
+            float: "left",
+            marginRight: "8px",
+          }}
+          src="https://flagcdn.com/w320/ca.png"
+          alt="Canada Flag"
+          width="32"
+          height="24"
+        />
+        <div>Fluent in English</div>
+      </div>
+      <div id="contactFormSalutation">
+        Send me an email if you have ideas for games or projects. I'm also open
+        to job opportunities.
+      </div>
+      <div className="contactPage">
+        <ContactForm></ContactForm>
+      </div>
     </div>
   );
 }
