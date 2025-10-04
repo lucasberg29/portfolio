2d4f335a2c1d46a6556778dd0762855945342675
lucasberg29 <lucasberg29@gmail.com>
2025-10-04 17:51:32 -0400

    added qr code to dev and fixed navbar buttons for mobile screens

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { qrcode } from 'vite-plugin-qrcode';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react()],
+    plugins: [react(), qrcode()],
     base: mode === "development" ? "" : "/portfolio",
     build: {
       outDir: "dist",
     },
     server: {
+      host: true,
       open: true,
     },
   };
